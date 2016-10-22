801b77cf6a5454531148232b0950ae33c6c84952
GruntBucket <simonsky42@gmail.com>
2016-10-21 20:26:32 -0700

    1

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,10 +1,12 @@
+import java.awt.*;
+
 /**
  * Created by Simon on 10/20/2016.
  */
-public class GameObject {
+public abstract class GameObject {
     protected int x,y;
     protected ID id;
-    protected int velX, VelY;
+    protected int velX, velY;
 
 
     public GameObject(int x, int y, ID id)
@@ -15,4 +17,44 @@ public class GameObject {
 
 
     }
+    public abstract void tick();
+    public abstract void render(Graphics g);
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+
+    }
+    public int getX(){
+        return x;
+
+    }
+    public int getY(){
+        return y;
+    }
+    public void setId(ID id){
+        this.id = id;
+
+    }
+    public ID getId(){
+        return id;
+    }
+    public void setVelX(int velX){
+        this.velX = velX;
+
+    }
+    public void setVelY(int velY){
+        this.velY = velY;
+
+    }
+    public int getVelX(){
+        return velX;
+    }
+    public int getVelY(){
+        return velY;
+
+    }
+
+
 }
